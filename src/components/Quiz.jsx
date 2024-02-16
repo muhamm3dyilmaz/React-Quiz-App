3be1322faf54749990ddd46648c98b1e508a4d2f
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-16 11:40:02 +0300

    splitting components & resolving bugs

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from '../questions.js';
-import QuestionTimer from "./QuestionTimer.jsx";
 import quizCompleteImage from '../assets/quiz-complete.png';
+import Question from "./Question.jsx";
 
 export default function Quiz() {
     const [answerState, setAnswerState] = useState('');
@@ -18,10 +18,10 @@ export default function Quiz() {
         });
 
         setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestion].answers[0]) {
+            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
                 setAnswerState('correct');
             } else {
-                selectedAnswer('wrong');
+                setAnswerState('wrong');
             }
 
             setTimeout(() => {
@@ -43,38 +43,17 @@ export default function Quiz() {
         );
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(() => Math.random() - 0.5);
-
     return (
         <div id="quiz">
-            <div id="question">
-                <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <ul id="answers">
-                    {shuffledAnswers.map((answer) => {
-                        const isSelected = userAnswers[userAnswers.length - 1] === answer;
-                        let cssClass = '';
-
-                        if (answerState === 'answered' && isSelected) {
-                            cssClass = 'selected';
-                        }
-
-                        if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-                            cssClass = answerState;
-                        }
-
-                        return (
-                            <li key={answer} className="answer">
-                                <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
-                                    {answer}
-                                </button>
-                            </li>
-                        );
-                    }
-                    )}
-                </ul>
-            </div>
+            <Question
+                key={activeQuestionIndex}
+                questionText={QUESTIONS[activeQuestionIndex].text}
+                answers={QUESTIONS[activeQuestionIndex].answers}
+                answerState={answerState}
+                selectedAnswer={userAnswers[userAnswers.length - 1]}
+                onSelectAnswer={handleSelectAnswer}
+                onSkipAnswer={handleSkipAnswer}
+            />
         </div>
     );
 }
